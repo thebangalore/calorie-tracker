ef5388fbacba93f7d7f3e4371c2730dd1f73d197
thebangalore <thebangalorepost4@gmail.com>
2026-01-10 02:15:51 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+const FOOD_DATA = {
+  rice: { calories: 360, protein: 7 },
+  oats: { calories: 380, protein: 13 },
+  chicken: { calories: 120, protein: 22 },
+  wholeEgg: { calories: 70, protein: 6 },
+  eggWhite: { calories: 17, protein: 4 },
+  whey: { calories: 120, protein: 24 },
+  gainer: { calories: 600, protein: 25 },
+  milk: { calories: 60, protein: 3 }
+};
+
+const TARGETS = {
+  training: 3000,
+  rest: 2500
+};
